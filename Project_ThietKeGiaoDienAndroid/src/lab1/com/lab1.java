1cf79fd8b5755cb9a28a4b18d12cf5459066cbd1
namdhpk00594 <namdhpk00594@fpt.edu.vn>
2016-09-20 08:31:39 +0700

    ThietKeGiaoDienAndroid

---
+++ Project_ThietKeGiaoDienAndroid/src/lab1/com/lab1.java
@@ -0,0 +1,15 @@
+package lab1.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+import manhinhdieukhien.com.R;
+
+public class lab1 extends Activity {// KHai bao dat
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) { // ****Mo mong xay nha =))***
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lab1);// set View cho lab1
+	}
+
+}
